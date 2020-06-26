9cf25b029dfaee94163a6ded460ccad4bfa53d33
Divya Bharathy Venkatraman <60822129+divyabharathy@users.noreply.github.com>
2020-06-26 08:53:34 +0530

    Create Inherit2.java

---
+++ Algorithms/Inherit2.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+abstract class Book {
+    String title;
+    String author;
+    
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    
+    abstract void display();
+}
+class MyBook extends Book{
+    int price;
+    MyBook(String t,String a,int p)
+    {
+        super(t,a);
+        this.price=p;
+    }
+    void display()
+    {
+        System.out.println("Title: "+super.title);
+        System.out.println("Author: "+super.author);
+        System.out.println("Price: "+price);
+    }
+}
+// Declare your class here. Do not use the 'public' access modifier.
+    // Declare the price instance variable
+    
+    /**   
+    *   Class Constructor
+    *   
+    *   @param title The book's title.
+    *   @param author The book's author.
+    *   @param price The book's price.
+    **/
+    // Write your constructor here
+    
+    /**   
+    *   Method Name: display
+    *   
+    *   Print the title, author, and price in the specified format.
+    **/
+    // Write your method here
+    
+    
+// End class
+
+public class Inherit2 {
+   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
