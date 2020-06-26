c51241c9bd1c85c9dce531c5daf5b20490e441a0
Divya Bharathy Venkatraman <60822129+divyabharathy@users.noreply.github.com>
2020-06-26 07:49:32 +0530

    Create HourGlassSum.java

---
+++ HourGlassSum.java
@@ -0,0 +1,87 @@
+/*
+SAMPLE INPUT
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+
+SAMPLE INPUT
+19
+
+EXPLANATION
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+
+The hourglass with the maximum sum (19) is:
+2 4 4
+  2
+1 2 4
+
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class HourGlassSum {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+        int sum=0,l=-100;
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<4;j++)
+            {
+                sum=0;
+                for(int k=0;k<3;k++)
+                {
+                    sum+=arr[i][j+k]+arr[i+2][j+k];
+                    if(k==1)
+                    {
+                        sum+=arr[i+1][j+k];
+                    }
+                }
+                if(sum>l)
+                {
+                    l=sum;
+                }
+            }
+        }
+        System.out.println(l);
+
+        scanner.close();
+    }
+}
